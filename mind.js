f1762a38999d1ae00783766d06d878c7abac51e9
25lp5076 User <25lp5076@administrators-iMac.local>
2025-12-26 13:11:28 +0800

    flip

--- mind.js
+++ mind.js
@@ -1,7 +1,20 @@
-const textChange=document.querySelector(".text")
-const boxChange=document.querySelector('.box')
+const cardFlip = document.querySelectorAll(".card");
 
+let emoji = ["🍀", "🍀", "🌷", "🌷"];
+let cardArr = [];
 
-boxChange.addEventListener("click", ()=>{
-    boxChange.classList.add('active');
-})
+function flipCard() {
+  this.classList.toggle("flip");
+  cardArr.push(this);
+  console.log(cardArr);
+}
+//this--> tuhain darsan element
+//toggle--> elementees hoornd class nemj hasna
+
+cardFlip.forEach((card) => {
+  card.addEventListener("click", flipCard);
+});
+//forEach--> element neg negeer songon avch(click hiihed songono) addEventListener nemj bga
+
+const shuffleEmoji = [...emoji].sort(() => 0.5 - Math.random());
+console.log(shuffleEmoji);
